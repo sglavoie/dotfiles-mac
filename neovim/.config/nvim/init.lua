0130337d6f74a483d55b36cef07437398a37d1e8
Sébastien Lavoie <sgdlavoie@gmail.com>
2024-02-05 09:19:39 -0600

    Remove plugin lukas-reineke/indent-blankline.nvim (breaking changes)

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -31,7 +31,6 @@ require('packer').startup(function(use)
 
   -- Prettify
   use 'nvim-lualine/lualine.nvim' -- Fancier statusline
-  use 'lukas-reineke/indent-blankline.nvim' -- Add indentation guides even on blank lines
   use({
     'rose-pine/neovim',
     as = 'rose-pine',
@@ -154,13 +153,6 @@ require('lualine').setup {
 
 -- Enable Comment.nvim
 require('Comment').setup()
-
--- Enable `lukas-reineke/indent-blankline.nvim`
--- See `:help indent_blankline.txt`
-require('indent_blankline').setup {
-  char = '┊',
-  show_trailing_blankline_indent = false,
-}
 
 -- Gitsigns
 -- See `:help gitsigns.txt`
