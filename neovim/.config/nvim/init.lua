37c84ba5e54541ff26aa3f8a3c5ed443b524c25c
Sébastien Lavoie <sgdlavoie@gmail.com>
2024-08-31 12:54:21 -0600

    nvim: make window split borders pop out visually
    Otherwise, they are pretty much invisible.

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -695,6 +695,7 @@ require('lazy').setup {
 
       -- You can configure highlights by doing something like
       vim.cmd.hi 'Comment gui=none'
+      vim.cmd.hi 'WinSeparator guifg=orange'
     end,
   },
 
